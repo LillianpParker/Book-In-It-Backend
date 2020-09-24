0ee14d96ef0236b848dbc80d3229efeedeb2f47a
Lillian Parker <Lillianpparker@outlook.com>
2020-09-24 12:04:55 -0500

    books are showing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://book-in-it.herokuapp.com/'
 
     resource '*',
       headers: :any,
